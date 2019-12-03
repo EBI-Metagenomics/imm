cbefc8e65872b3629d29d5c9a8213c2a90dcc9f5
Danilo Horta <danilo.horta@pm.me>
2019-12-03 12:29:56 +0000

    fix [-Wtype-limits]

--- src/ascii.h
+++ src/ascii.h
@@ -8,7 +8,7 @@
 static inline int ascii_is_std(char const* str, size_t len)
 {
     for (size_t i = 0; i < len; ++i) {
-        if (str[i] < 0 || str[i] > 127)
+        if (str[i] < 0)
             return 0;
     }
     return 1;
