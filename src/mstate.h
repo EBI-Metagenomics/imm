400c0eb7afe68e7c2ce6242173e773cf1ea16988
Danilo Horta <danilo.horta@pm.me>
2020-03-23 11:09:11 +0000

    indent

--- src/mstate.h
+++ src/mstate.h
@@ -5,11 +5,11 @@ struct imm_state;
 struct mstate;
 struct mtrans_table;
 
-struct mstate*             mstate_create(struct imm_state const* state, double start_lprob);
-void                       mstate_destroy(struct mstate* mstate);
-struct imm_state const*    mstate_get_state(struct mstate const* mstate);
-double                     mstate_get_start(struct mstate const* mstate);
-void                       mstate_set_start(struct mstate* mstate, double lprob);
-struct mtrans_table * mstate_get_mtrans_table(struct mstate const* mstate);
+struct mstate*          mstate_create(struct imm_state const* state, double start_lprob);
+void                    mstate_destroy(struct mstate* mstate);
+struct imm_state const* mstate_get_state(struct mstate const* mstate);
+double                  mstate_get_start(struct mstate const* mstate);
+void                    mstate_set_start(struct mstate* mstate, double lprob);
+struct mtrans_table*    mstate_get_mtrans_table(struct mstate const* mstate);
 
 #endif
