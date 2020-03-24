f731c9c1db768ddf1384673f50d351bf08f676ef
Danilo Horta <danilo.horta@pm.me>
2020-03-24 10:04:31 +0000

    trans_table ok

--- src/dp_draft/trans_table.c
+++ src/dp_draft/trans_table.c
@@ -1,4 +1,4 @@
-#include "dp/trans_table.h"
+#include "dp_draft/trans_table.h"
 #include "free.h"
 #include "mstate.h"
 #include "mtrans.h"
@@ -6,53 +6,46 @@
 #include "state_idx.h"
 #include <stdlib.h>
 
-struct dp_trans_table
-{
-    double*   cost;
-    unsigned* source_state;
-    unsigned* offset;
-};
-
 struct dp_trans_table const* dp_trans_table_create(struct mstate const* const* mstates,
                                                    unsigned                    nstates,
                                                    struct state_idx*           state_idx)
 {
-    struct dp_trans_table* t = malloc(sizeof(struct dp_trans_table));
-    t->offset = malloc(sizeof(unsigned) * nstates);
-    t->offset[0] = 0;
+    struct dp_trans_table* trans_tbl = malloc(sizeof(struct dp_trans_table));
+    trans_tbl->offset = malloc(sizeof(unsigned) * (nstates + 1));
+    trans_tbl->offset[0] = 0;
 
     unsigned ntrans = mtrans_table_size(mstate_get_mtrans_table(mstates[0]));
     for (unsigned i = 1; i < nstates; ++i) {
-        t->offset[i] = ntrans;
+        trans_tbl->offset[i] = ntrans;
         ntrans += mtrans_table_size(mstate_get_mtrans_table(mstates[i]));
     }
+    /* used for (offset[i+1] - offset[i+1]) calculation */
+    trans_tbl->offset[nstates] = ntrans;
 
-    t->source_state = malloc(sizeof(unsigned) * ntrans);
+    trans_tbl->target_state = malloc(sizeof(unsigned) * ntrans);
 
     for (unsigned i = 0; i < nstates; ++i) {
         struct mtrans_table const* tbl = mstate_get_mtrans_table(mstates[i]);
         unsigned long              iter = 0;
         unsigned                   j = 0;
         mtrans_table_for_each (iter, tbl) {
-            if (mtrans_table_exist(tbl, iter)) {
-                struct mtrans* mtrans = mtrans_table_get(tbl, iter);
-                t->cost[t->offset[i] + j] = mtrans_get_lprob(mtrans);
-                t->source_state[t->offset[i] + j] =
-                    state_idx_find(state_idx, mtrans_get_state(mtrans));
-                ++j;
-            }
+            if (!mtrans_table_exist(tbl, iter))
+                continue;
+
+            struct mtrans* mtrans = mtrans_table_get(tbl, iter);
+            trans_tbl->cost[trans_tbl->offset[i] + j] = mtrans_get_lprob(mtrans);
+            trans_tbl->target_state[trans_tbl->offset[i] + j] =
+                state_idx_find(state_idx, mtrans_get_state(mtrans));
+            ++j;
         }
     }
 
-    return t;
+    return trans_tbl;
 }
 
-/* double dp_trans_table_cost(struct dp_trans_table const* transition, unsigned source_state)
- */
-
-void dp_trans_table_destroy(struct dp_trans_table const* transition)
+void dp_trans_table_destroy(struct dp_trans_table const* trans_tbl)
 {
-    imm_free(transition->cost);
-    imm_free(transition->source_state);
-    imm_free(transition);
+    imm_free(trans_tbl->cost);
+    imm_free(trans_tbl->target_state);
+    imm_free(trans_tbl);
 }
--- src/dp_draft/trans_table.h
+++ src/dp_draft/trans_table.h
@@ -4,9 +4,35 @@
 struct mstate;
 struct state_idx;
 
+struct dp_trans_table
+{
+    double*   cost;
+    unsigned* target_state;
+    unsigned* offset;
+};
+
 struct dp_trans_table const* dp_trans_table_create(struct mstate const* const* mstates,
                                                    unsigned                    nstates,
                                                    struct state_idx*           state_idx);
-void                         dp_trans_table_destroy(struct dp_trans_table const* transition);
+
+static inline double dp_trans_table_ntrans(struct dp_trans_table const* trans_tbl,
+                                           unsigned                     source_state)
+{
+    return trans_tbl->offset[source_state + 1] - trans_tbl->offset[source_state];
+}
+
+static inline double dp_trans_table_cost(struct dp_trans_table const* trans_tbl,
+                                         unsigned source_state, unsigned trans_idx)
+{
+    return trans_tbl->cost[trans_tbl->offset[source_state] + trans_idx];
+}
+
+static inline unsigned dp_trans_table_target_state(struct dp_trans_table const* trans_tbl,
+                                                   unsigned source_state, unsigned trans_idx)
+{
+    return trans_tbl->target_state[trans_tbl->offset[source_state] + trans_idx];
+}
+
+void dp_trans_table_destroy(struct dp_trans_table const* transition);
 
 #endif
