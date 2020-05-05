2d8768a0d0e8dd9a59424f1dea25245358b91a27
Danilo Horta <danilo.horta@pm.me>
2020-05-05 15:47:58 +0100

    Format bug.h

--- include/imm/bug.h
+++ include/imm/bug.h
@@ -4,13 +4,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define IMM_BUG(cond)                                                                             \
-    do {                                                                                      \
-        if (!(cond))                                                                          \
-            break;                                                                            \
-        fprintf(stderr, "BUG: %s: %s: %d: %s\n", __FILE__, __func__, __LINE__, #cond);        \
-        fflush(stderr);                                                                       \
-        exit(1);                                                                              \
+#define IMM_BUG(cond)                                                                              \
+    do {                                                                                           \
+        if (!(cond))                                                                               \
+            break;                                                                                 \
+        fprintf(stderr, "BUG: %s: %s: %d: %s\n", __FILE__, __func__, __LINE__, #cond);             \
+        fflush(stderr);                                                                            \
+        exit(1);                                                                                   \
     } while (0)
 
 #endif
