a078898417e032abbfb25fef614fc9fe943cca46
Danilo Horta <danilo.horta@pm.me>
2020-05-06 16:36:43 +0100

    Results clean-up

--- include/imm/results.h
+++ include/imm/results.h
@@ -4,26 +4,22 @@
 #include "imm/export.h"
 #include "imm/subseq.h"
 
-struct imm_seq;
-struct imm_results;
 struct imm_path;
 struct imm_result;
+struct imm_results;
+struct imm_seq;
 
-IMM_EXPORT struct imm_results*      imm_results_create(struct imm_seq const* seq,
-                                                       unsigned              nresults);
-IMM_EXPORT void                     imm_results_set(struct imm_results* results, unsigned idx,
-                                                    struct imm_subseq subseq, struct imm_path const* path,
-                                                    double loglik);
-IMM_EXPORT struct imm_result const* imm_results_get(struct imm_results const* results,
-                                                    unsigned                  idx);
-IMM_EXPORT unsigned                 imm_results_size(struct imm_results const* results);
-IMM_EXPORT void                     imm_results_destroy(struct imm_results const* results);
-IMM_EXPORT void                     imm_results_free(struct imm_results const* results);
-
+IMM_EXPORT void                   imm_result_destroy(struct imm_result const* result);
 IMM_EXPORT double                 imm_result_loglik(struct imm_result const* result);
 IMM_EXPORT struct imm_path const* imm_result_path(struct imm_result const* result);
 IMM_EXPORT struct imm_subseq      imm_result_subseq(struct imm_result const* result);
-IMM_EXPORT void                   imm_result_destroy(struct imm_result const* result);
-IMM_EXPORT void                   imm_result_free(struct imm_result const* result);
+
+IMM_EXPORT struct imm_results* imm_results_create(struct imm_seq const* seq, unsigned nresults);
+IMM_EXPORT void                imm_results_destroy(struct imm_results const* results);
+IMM_EXPORT struct imm_result const* imm_results_get(struct imm_results const* results,
+                                                    unsigned                  idx);
+IMM_EXPORT void imm_results_set(struct imm_results* results, unsigned idx, struct imm_subseq subseq,
+                                struct imm_path const* path, double loglik);
+IMM_EXPORT unsigned imm_results_size(struct imm_results const* results);
 
 #endif
--- src/results.c
+++ src/results.c
@@ -2,7 +2,6 @@
 #include "free.h"
 #include "imm/path.h"
 #include "imm/subseq.h"
-#include "imm/window.h"
 #include <stdlib.h>
 
 struct imm_result
@@ -19,17 +18,41 @@ struct imm_results
     struct imm_result*    result[];
 };
 
+void imm_result_destroy(struct imm_result const* result)
+{
+    imm_path_destroy(result->path);
+    free_c(result);
+}
+
+double imm_result_loglik(struct imm_result const* result) { return result->loglik; }
+
+struct imm_path const* imm_result_path(struct imm_result const* result) { return result->path; }
+
+struct imm_subseq imm_result_subseq(struct imm_result const* result) { return result->subseq; }
+
 struct imm_results* imm_results_create(struct imm_seq const* seq, unsigned const nresults)
 {
-    struct imm_results* results =
-        malloc(sizeof(struct imm_results) + sizeof(struct imm_result* [nresults]));
+    struct imm_results* results = malloc(sizeof(*results) + sizeof(struct imm_result* [nresults]));
 
     results->seq = seq;
     results->nresults = nresults;
     for (unsigned i = 0; i < results->nresults; ++i)
-        results->result[i] = malloc(sizeof(struct imm_result));
+        results->result[i] = malloc(sizeof(*results->result[i]));
 
     return results;
+}
+
+void imm_results_destroy(struct imm_results const* results)
+{
+    for (unsigned i = 0; i < results->nresults; ++i)
+        imm_result_destroy(results->result[i]);
+
+    free_c(results);
+}
+
+struct imm_result const* imm_results_get(struct imm_results const* results, unsigned idx)
+{
+    return results->result[idx];
 }
 
 void imm_results_set(struct imm_results* results, unsigned idx, struct imm_subseq subseq,
@@ -40,36 +63,4 @@ void imm_results_set(struct imm_results* results, unsigned idx, struct imm_subse
     results->result[idx]->loglik = loglik;
 }
 
-struct imm_result const* imm_results_get(struct imm_results const* results, unsigned idx)
-{
-    return results->result[idx];
-}
-
 unsigned imm_results_size(struct imm_results const* results) { return results->nresults; }
-
-void imm_results_destroy(struct imm_results const* results)
-{
-    for (unsigned i = 0; i < results->nresults; ++i)
-        imm_result_destroy(results->result[i]);
-
-    imm_results_free(results);
-}
-
-void imm_results_free(struct imm_results const* results) { free_c(results); }
-
-double imm_result_loglik(struct imm_result const* result) { return result->loglik; }
-
-struct imm_path const* imm_result_path(struct imm_result const* result)
-{
-    return result->path;
-}
-
-struct imm_subseq imm_result_subseq(struct imm_result const* result) { return result->subseq; }
-
-void imm_result_destroy(struct imm_result const* result)
-{
-    imm_path_destroy(result->path);
-    imm_result_free(result);
-}
-
-void imm_result_free(struct imm_result const* result) { free_c(result); }
