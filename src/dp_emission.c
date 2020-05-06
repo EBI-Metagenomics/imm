489f492999790bd311457a874b0dff2d44e22b0b
Danilo Horta <danilo.horta@pm.me>
2020-05-06 17:51:23 +0100

    dp_emission clean-up

--- src/dp_emission.c
+++ src/dp_emission.c
@@ -18,15 +18,11 @@ struct dp_emission_chunk
     uint32_t* offset;
 };
 
-static inline unsigned score_size(struct dp_emission const* emission, uint32_t nstates)
-{
-    return emission->offset[nstates];
-}
 static inline uint32_t offset_size(uint32_t nstates) { return nstates + 1; }
+static inline unsigned score_size(struct dp_emission const* emission, uint32_t nstates);
 
 struct dp_emission const* dp_emission_create(struct seq_code const*      seq_code,
-                                             struct mstate const* const* mstates,
-                                             uint32_t                    nstates)
+                                             struct mstate const* const* mstates, uint32_t nstates)
 {
     struct dp_emission* emiss_tbl = malloc(sizeof(*emiss_tbl));
 
@@ -45,7 +41,7 @@ struct dp_emission const* dp_emission_create(struct seq_code const*      seq_cod
     struct imm_abc const* abc = seq_code_abc(seq_code);
     char const*           set = imm_abc_symbols(abc);
     unsigned              set_size = imm_abc_length(abc);
-    struct imm_cartes* cartes = imm_cartes_create(set, set_size, seq_code_max_seq(seq_code));
+    struct imm_cartes*    cartes = imm_cartes_create(set, set_size, seq_code_max_seq(seq_code));
 
     for (uint32_t i = 0; i < nstates; ++i) {
 
@@ -77,30 +73,6 @@ void dp_emission_destroy(struct dp_emission const* emission)
     free_c(emission);
 }
 
-int dp_emission_write(struct dp_emission const* emission, uint32_t nstates, FILE* stream)
-{
-    struct dp_emission_chunk chunk = {.score_size = score_size(emission, nstates),
-                                      .score = emission->score,
-                                      .offset_size = offset_size(nstates),
-                                      .offset = emission->offset};
-
-    if (fwrite(&chunk.score_size, sizeof(chunk.score_size), 1, stream) < 1)
-        return 1;
-
-    if (fwrite(chunk.score, sizeof(*chunk.score), score_size(emission, nstates), stream) <
-        score_size(emission, nstates))
-        return 1;
-
-    if (fwrite(&chunk.offset_size, sizeof(chunk.offset_size), 1, stream) < 1)
-        return 1;
-
-    if (fwrite(chunk.offset, sizeof(*chunk.offset), offset_size(nstates), stream) <
-        offset_size(nstates))
-        return 1;
-
-    return 0;
-}
-
 struct dp_emission const* dp_emission_read(FILE* stream)
 {
     struct dp_emission_chunk chunk = {
@@ -117,8 +89,7 @@ struct dp_emission const* dp_emission_read(FILE* stream)
 
     chunk.score = malloc(sizeof(*chunk.score) * chunk.score_size);
 
-    if (fread(chunk.score, sizeof(*chunk.score), chunk.score_size, stream) <
-        chunk.score_size) {
+    if (fread(chunk.score, sizeof(*chunk.score), chunk.score_size, stream) < chunk.score_size) {
         imm_error("could not read score");
         goto err;
     }
@@ -130,8 +101,7 @@ struct dp_emission const* dp_emission_read(FILE* stream)
 
     chunk.offset = malloc(sizeof(*chunk.offset) * chunk.offset_size);
 
-    if (fread(chunk.offset, sizeof(*chunk.offset), chunk.offset_size, stream) <
-        chunk.offset_size) {
+    if (fread(chunk.offset, sizeof(*chunk.offset), chunk.offset_size, stream) < chunk.offset_size) {
         imm_error("could not read offset");
         goto err;
     }
@@ -151,4 +121,41 @@ err:
     free_c(emission);
 
     return NULL;
+}
+
+int dp_emission_write(struct dp_emission const* emission, uint32_t nstates, FILE* stream)
+{
+    struct dp_emission_chunk chunk = {.score_size = score_size(emission, nstates),
+                                      .score = emission->score,
+                                      .offset_size = offset_size(nstates),
+                                      .offset = emission->offset};
+
+    if (fwrite(&chunk.score_size, sizeof(chunk.score_size), 1, stream) < 1) {
+        imm_error("could not write score_size");
+        return 1;
+    }
+
+    if (fwrite(chunk.score, sizeof(*chunk.score), score_size(emission, nstates), stream) <
+        score_size(emission, nstates)) {
+        imm_error("could not write score");
+        return 1;
+    }
+
+    if (fwrite(&chunk.offset_size, sizeof(chunk.offset_size), 1, stream) < 1) {
+        imm_error("could not write offset_size");
+        return 1;
+    }
+
+    if (fwrite(chunk.offset, sizeof(*chunk.offset), offset_size(nstates), stream) <
+        offset_size(nstates)) {
+        imm_error("could not write offset");
+        return 1;
+    }
+
+    return 0;
+}
+
+static inline unsigned score_size(struct dp_emission const* emission, uint32_t nstates)
+{
+    return emission->offset[nstates];
 }
--- src/dp_emission.h
+++ src/dp_emission.h
@@ -15,18 +15,17 @@ struct dp_emission
 };
 
 struct dp_emission const* dp_emission_create(struct seq_code const*      seq_code,
-                                             struct mstate const* const* mstates,
-                                             uint32_t                    nstates);
+                                             struct mstate const* const* mstates, uint32_t nstates);
+void                      dp_emission_destroy(struct dp_emission const* emission);
+struct dp_emission const* dp_emission_read(FILE* stream);
+static inline double      dp_emission_score(struct dp_emission const* emission, uint32_t state,
+                                            unsigned seq_code);
+int dp_emission_write(struct dp_emission const* emission, uint32_t nstates, FILE* stream);
 
 static inline double dp_emission_score(struct dp_emission const* emission, uint32_t state,
                                        unsigned seq_code)
 {
     return emission->score[emission->offset[state] + seq_code];
 }
-
-void dp_emission_destroy(struct dp_emission const* emission);
-
-int dp_emission_write(struct dp_emission const* emission, uint32_t nstates, FILE* stream);
-struct dp_emission const* dp_emission_read(FILE* stream);
 
 #endif
