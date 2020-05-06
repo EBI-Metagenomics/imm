7176b8d5aeb9ed06c0c9ede3ccfd1c2c7a4e97a4
Danilo Horta <danilo.horta@pm.me>
2020-05-06 16:26:28 +0100

    Report sort funcs

--- src/report.c
+++ src/report.c
@@ -2,15 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void imm_error(char const* err, ...)
-{
-    va_list params;
-    va_start(params, err);
-    vfprintf(stderr, err, params);
-    fputc('\n', stderr);
-    va_end(params);
-}
-
 void imm_die(char const* err, ...)
 {
     va_list params;
@@ -18,4 +9,13 @@ void imm_die(char const* err, ...)
     imm_error(err, params);
     va_end(params);
     exit(1);
+}
+
+void imm_error(char const* err, ...)
+{
+    va_list params;
+    va_start(params, err);
+    vfprintf(stderr, err, params);
+    fputc('\n', stderr);
+    va_end(params);
 }
