00b3020213e977987fcc017dd7d60de6220aa104
Danilo Horta <danilo.horta@pm.me>
2020-05-06 16:41:15 +0100

    Seq clean-up

--- include/imm/seq.h
+++ include/imm/seq.h
@@ -6,7 +6,6 @@
 struct imm_abc;
 
 /**
- *
  * Sequence of characters. No need for null-terminated string.
  */
 struct imm_seq
@@ -16,17 +15,16 @@ struct imm_seq
     unsigned              length;
 };
 
-IMM_EXPORT struct imm_seq const*    imm_seq_create(char const* seq, struct imm_abc const* abc);
-static inline struct imm_abc const* imm_seq_get_abc(struct imm_seq const* seq)
-{
-    return seq->abc;
-}
+static inline struct imm_seq        IMM_SEQ(struct imm_abc const* abc, char const* string,
+                                            unsigned length);
+IMM_EXPORT struct imm_seq const*    imm_seq_clone(struct imm_seq const* seq);
+IMM_EXPORT struct imm_seq const*    imm_seq_create(char const* string, struct imm_abc const* abc);
+IMM_EXPORT void                     imm_seq_destroy(struct imm_seq const* seq);
+static inline struct imm_abc const* imm_seq_get_abc(struct imm_seq const* seq) { return seq->abc; }
 static inline unsigned    imm_seq_length(struct imm_seq const* seq) { return seq->length; }
 static inline char const* imm_seq_string(struct imm_seq const* seq) { return seq->string; }
-IMM_EXPORT struct imm_seq const* imm_seq_clone(struct imm_seq const* seq);
-IMM_EXPORT void                  imm_seq_destroy(struct imm_seq const* seq);
-static inline struct imm_seq     IMM_SEQ(struct imm_abc const* abc, char const* string,
-                                         unsigned length)
+
+static inline struct imm_seq IMM_SEQ(struct imm_abc const* abc, char const* string, unsigned length)
 {
     return (struct imm_seq){abc, string, length};
 }
--- src/seq.c
+++ src/seq.c
@@ -5,35 +5,35 @@
 #include <limits.h>
 #include <string.h>
 
-struct imm_seq const* imm_seq_create(char const* seq, struct imm_abc const* abc)
+struct imm_seq const* imm_seq_clone(struct imm_seq const* seq)
 {
-    size_t length = strlen(seq);
+    struct imm_seq* new_seq = malloc(sizeof(*new_seq));
+    new_seq->abc = seq->abc;
+    new_seq->string = strndup(seq->string, seq->length);
+    new_seq->length = seq->length;
+    return new_seq;
+}
+
+struct imm_seq const* imm_seq_create(char const* string, struct imm_abc const* abc)
+{
+    size_t length = strlen(string);
     if (length > UINT_MAX) {
         imm_error("sequence is too long");
         return NULL;
     }
 
     for (unsigned i = 0; i < length; ++i) {
-        if (!imm_abc_has_symbol(abc, seq[i]) && seq[i] != imm_abc_any_symbol(abc)) {
+        if (!imm_abc_has_symbol(abc, string[i]) && string[i] != imm_abc_any_symbol(abc)) {
             imm_error("symbol not found in the alphabet");
             return NULL;
         }
     }
 
-    struct imm_seq* ret = malloc(sizeof(struct imm_seq));
-    ret->abc = abc;
-    ret->string = strdup(seq);
-    ret->length = (unsigned)length;
-    return ret;
-}
-
-struct imm_seq const* imm_seq_clone(struct imm_seq const* seq)
-{
-    struct imm_seq* ret = malloc(sizeof(struct imm_seq));
-    ret->abc = seq->abc;
-    ret->string = strndup(seq->string, seq->length);
-    ret->length = seq->length;
-    return ret;
+    struct imm_seq* seq = malloc(sizeof(*seq));
+    seq->abc = abc;
+    seq->string = strdup(string);
+    seq->length = (unsigned)length;
+    return seq;
 }
 
 void imm_seq_destroy(struct imm_seq const* seq)
