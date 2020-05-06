cce9f0f2ba414435d6ca9bf83ed4cfb8801a6620
Danilo Horta <danilo.horta@pm.me>
2020-05-06 15:25:55 +0100

    lprob sort prototypes

--- include/imm/lprob.h
+++ include/imm/lprob.h
@@ -7,19 +7,14 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-static inline double imm_lprob_zero(void) { return -INFINITY; }
-static inline bool   imm_lprob_is_zero(double a) { return isinf(a) && a < 0.0; }
-static inline bool   imm_lprob_is_valid(double a) { return !isnan(a); }
-static inline double imm_lprob_invalid(void) { return NAN; }
 static inline double imm_lprob_add(double a, double b) { return logaddexp(a, b); }
-static inline double imm_lprob_sum(double const* arr, size_t len)
-{
+static inline double imm_lprob_invalid(void) { return NAN; }
+static inline bool   imm_lprob_is_valid(double a) { return !isnan(a); }
+static inline bool   imm_lprob_is_zero(double a) { return isinf(a) && a < 0.0; }
+static inline int    imm_lprob_normalize(double* arr, size_t len);
+static inline double imm_lprob_sum(double const* arr, size_t len);
+static inline double imm_lprob_zero(void) { return -INFINITY; }
 
-    double r = imm_lprob_zero();
-    for (size_t i = 0; i < len; ++i)
-        r = logaddexp(r, arr[i]);
-    return r;
-}
 static inline int imm_lprob_normalize(double* arr, size_t len)
 {
     double lnorm = imm_lprob_sum(arr, len);
@@ -32,6 +27,15 @@ static inline int imm_lprob_normalize(double* arr, size_t len)
         arr[i] -= lnorm;
 
     return 0;
+}
+
+static inline double imm_lprob_sum(double const* arr, size_t len)
+{
+
+    double r = imm_lprob_zero();
+    for (size_t i = 0; i < len; ++i)
+        r = logaddexp(r, arr[i]);
+    return r;
 }
 
 #endif
