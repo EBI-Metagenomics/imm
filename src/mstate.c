b135a8b81b806111b5603e57c25588e38640fe94
Danilo Horta <danilo.horta@pm.me>
2020-05-06 20:34:08 +0100

    mstate clean-up

--- src/mstate.c
+++ src/mstate.c
@@ -1,21 +1,10 @@
 #include "mstate.h"
-#include "cast.h"
 #include "free.h"
-#include "imm/io.h"
-#include "imm/state.h"
-#include "io.h"
-#include "mtrans.h"
 #include "mtrans_table.h"
-#include "state.h"
-#include <limits.h>
-#include <math.h>
-#include <string.h>
-
-static int mstate_write(struct mstate const* mstate, FILE* stream);
 
 struct mstate* mstate_create(struct imm_state const* state, double start_lprob)
 {
-    struct mstate* mstate = malloc(sizeof(struct mstate));
+    struct mstate* mstate = malloc(sizeof(*mstate));
     mstate->state = state;
     mstate->start_lprob = start_lprob;
     mstate->mtrans_table = mtrans_table_create();
@@ -28,11 +17,11 @@ void mstate_destroy(struct mstate* mstate)
     free_c(mstate);
 }
 
-double mstate_get_start(struct mstate const* mstate) { return mstate->start_lprob; }
-
-void mstate_set_start(struct mstate* mstate, double const lprob) { mstate->start_lprob = lprob; }
-
 struct mtrans_table* mstate_get_mtrans_table(struct mstate const* mstate)
 {
     return mstate->mtrans_table;
 }
+
+double mstate_get_start(struct mstate const* mstate) { return mstate->start_lprob; }
+
+void mstate_set_start(struct mstate* mstate, double const lprob) { mstate->start_lprob = lprob; }
--- src/mstate.h
+++ src/mstate.h
@@ -16,14 +16,16 @@ struct mstate
     struct mtrans_table*    mtrans_table;
 };
 
-struct mstate* mstate_create(struct imm_state const* state, double start_lprob);
-void           mstate_destroy(struct mstate* mstate);
+struct mstate*       mstate_create(struct imm_state const* state, double start_lprob);
+void                 mstate_destroy(struct mstate* mstate);
+struct mtrans_table* mstate_get_mtrans_table(struct mstate const* mstate);
+double               mstate_get_start(struct mstate const* mstate);
+static inline struct imm_state const* mstate_get_state(struct mstate const* mstate);
+void                                  mstate_set_start(struct mstate* mstate, double lprob);
+
 static inline struct imm_state const* mstate_get_state(struct mstate const* mstate)
 {
     return mstate->state;
 }
-double               mstate_get_start(struct mstate const* mstate);
-void                 mstate_set_start(struct mstate* mstate, double lprob);
-struct mtrans_table* mstate_get_mtrans_table(struct mstate const* mstate);
 
 #endif
