484c8797f437d267fa948a47a9a71728ef9dd931
Danilo Horta <danilo.horta@pm.me>
2021-02-19 00:37:01 +0000

    IMM_API

--- include/imm/file.h
+++ include/imm/file.h
@@ -1,10 +1,11 @@
 #ifndef IMM_FILE_H
 #define IMM_FILE_H
 
+#include "imm/export.h"
 #include <stdint.h>
 #include <stdio.h>
 
-int     imm_file_seek(FILE* stream, int64_t offset, int origin);
-int64_t imm_file_tell(FILE* stream);
+IMM_API int     imm_file_seek(FILE* stream, int64_t offset, int origin);
+IMM_API int64_t imm_file_tell(FILE* stream);
 
 #endif
