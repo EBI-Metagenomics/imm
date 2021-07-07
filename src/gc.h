1413b4571f29f8644e158359d47718aba52aa390
Danilo Horta <danilo.horta@pm.me>
2021-07-07 03:53:01 +0100

    auto generate genetic code

---
+++ src/gc.h
@@ -0,0 +1,16 @@
+#ifndef IMM_GC_H
+#define IMM_GC_H
+
+struct gc
+{
+    char const *name1;
+    char const *name2;
+    unsigned const id;
+    char const *ncbieaa;
+    char const *sncbieaa;
+    char const *base1;
+    char const *base2;
+    char const *base3;
+};
+
+#endif
