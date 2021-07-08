f9b1c14ec56afa8b09d3dee18760bc04fa4d127e
Danilo Horta <danilo.horta@pm.me>
2021-07-08 11:36:13 +0100

    cmake-format

--- test/gc.c
+++ test/gc.c
@@ -2,10 +2,6 @@
 #include "imm/imm.h"
 
 void test_gc(void);
-void test_abc_duplicated_alphabet(void);
-void test_abc_duplicated_any_symbol(void);
-void test_abc_symbol_outside_range(void);
-void test_abc_any_symbol_outside_range(void);
 
 int main(void)
 {
@@ -26,9 +22,4 @@ void test_gc(void)
     EQ(imm_codon_asym(codons + 1), 'T');
     EQ(imm_codon_bsym(codons + 1), 'T');
     EQ(imm_codon_csym(codons + 1), 'C');
-    /* EQ(codons[0].b, imm_abc_symbol_idx(&codons[0].nuclt->super, 'T')); */
-    /* EQ(codons[0].c, imm_abc_symbol_idx(&codons[0].nuclt->super, 'T')); */
-    /* EQ(codons[1].a, imm_abc_symbol_idx(&codons[0].nuclt->super, 'T')); */
-    /* EQ(codons[1].b, imm_abc_symbol_idx(&codons[0].nuclt->super, 'T')); */
-    /* EQ(codons[1].c, imm_abc_symbol_idx(&codons[0].nuclt->super, 'C')); */
 }
