c8aecf4aa5a77bd215596918810e0d25c60a337b
Danilo Horta <danilo.horta@pm.me>
2021-07-08 19:45:33 +0100

    Genetic code testing.

--- test/gc.c
+++ test/gc.c
@@ -1,15 +1,19 @@
 #include "hope/hope.h"
 #include "imm/imm.h"
 
-void test_gc(void);
+void test_gc_f(void);
+void test_gc_l(void);
+void test_gc_p(void);
 
 int main(void)
 {
-    test_gc();
+    test_gc_f();
+    test_gc_l();
+    test_gc_p();
     return hope_status();
 }
 
-void test_gc(void)
+void test_gc_f(void)
 {
     EQ(imm_gc_name1(1), "Standard");
     EQ(imm_gc_name2(1), "SGC0");
@@ -22,4 +26,57 @@ void test_gc(void)
     EQ(imm_codon_asym(codons + 1), 'T');
     EQ(imm_codon_bsym(codons + 1), 'T');
     EQ(imm_codon_csym(codons + 1), 'C');
+}
+
+void test_gc_l(void)
+{
+    EQ(imm_gc_name1(1), "Standard");
+    EQ(imm_gc_name2(1), "SGC0");
+    EQ(imm_gc_ncodons(1, 'L'), 6);
+    struct imm_codon codons[6] = {
+        imm_gc_codon(1, 'L', 0), imm_gc_codon(1, 'L', 1),
+        imm_gc_codon(1, 'L', 2), imm_gc_codon(1, 'L', 3),
+        imm_gc_codon(1, 'L', 4), imm_gc_codon(1, 'L', 5)};
+
+    EQ(imm_codon_asym(codons + 0), 'T');
+    EQ(imm_codon_bsym(codons + 0), 'T');
+    EQ(imm_codon_csym(codons + 0), 'A');
+    EQ(imm_codon_asym(codons + 1), 'T');
+    EQ(imm_codon_bsym(codons + 1), 'T');
+    EQ(imm_codon_csym(codons + 1), 'G');
+    EQ(imm_codon_asym(codons + 2), 'C');
+    EQ(imm_codon_bsym(codons + 2), 'T');
+    EQ(imm_codon_csym(codons + 2), 'T');
+    EQ(imm_codon_asym(codons + 3), 'C');
+    EQ(imm_codon_bsym(codons + 3), 'T');
+    EQ(imm_codon_csym(codons + 3), 'C');
+    EQ(imm_codon_asym(codons + 4), 'C');
+    EQ(imm_codon_bsym(codons + 4), 'T');
+    EQ(imm_codon_csym(codons + 4), 'A');
+    EQ(imm_codon_asym(codons + 5), 'C');
+    EQ(imm_codon_bsym(codons + 5), 'T');
+    EQ(imm_codon_csym(codons + 5), 'G');
+}
+
+void test_gc_p(void)
+{
+    EQ(imm_gc_name1(1), "Standard");
+    EQ(imm_gc_name2(1), "SGC0");
+    EQ(imm_gc_ncodons(1, 'P'), 4);
+    struct imm_codon codons[4] = {
+        imm_gc_codon(1, 'P', 0), imm_gc_codon(1, 'P', 1),
+        imm_gc_codon(1, 'P', 2), imm_gc_codon(1, 'P', 3)};
+
+    EQ(imm_codon_asym(codons + 0), 'C');
+    EQ(imm_codon_bsym(codons + 0), 'C');
+    EQ(imm_codon_csym(codons + 0), 'T');
+    EQ(imm_codon_asym(codons + 1), 'C');
+    EQ(imm_codon_bsym(codons + 1), 'C');
+    EQ(imm_codon_csym(codons + 1), 'C');
+    EQ(imm_codon_asym(codons + 2), 'C');
+    EQ(imm_codon_bsym(codons + 2), 'C');
+    EQ(imm_codon_csym(codons + 2), 'A');
+    EQ(imm_codon_asym(codons + 3), 'C');
+    EQ(imm_codon_bsym(codons + 3), 'C');
+    EQ(imm_codon_csym(codons + 3), 'G');
 }
