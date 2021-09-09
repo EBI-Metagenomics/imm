f44d967c5717769aee3ff4c4414ea61a30fc59eb
Danilo Horta <danilo.horta@pm.me>
2021-09-09 11:26:05 +0100

    typos

--- src/dp/emis.c
+++ src/dp/emis.c
@@ -13,37 +13,36 @@ void emis_del(struct imm_dp_emis const *emis)
     free(emis->offset);
 }
 
-void emis_init(struct imm_dp_emis *emiss)
+void emis_init(struct imm_dp_emis *emis)
 {
-    emiss->score = NULL;
-    emiss->offset = NULL;
+    emis->score = NULL;
+    emis->offset = NULL;
 }
 
-enum imm_rc emis_reset(struct imm_dp_emis *emiss,
-                       struct imm_dp_code const *code,
+enum imm_rc emis_reset(struct imm_dp_emis *emis, struct imm_dp_code const *code,
                        struct imm_state **states, unsigned nstates)
 {
     unsigned offsize = emis_offset_size(nstates);
-    emiss->offset = realloc(emiss->offset, sizeof(*emiss->offset) * offsize);
-    if (!emiss->offset && offsize > 0)
+    emis->offset = realloc(emis->offset, sizeof(*emis->offset) * offsize);
+    if (!emis->offset && offsize > 0)
         return error(IMM_OUTOFMEM, "failed to resize");
-    emiss->offset[0] = 0;
+    emis->offset[0] = 0;
 
     unsigned min = imm_state_span(states[0]).min;
     unsigned size = code_size(code, min);
     for (unsigned i = 1; i < nstates; ++i)
     {
-        emiss->offset[i] = size;
+        emis->offset[i] = size;
         size += code_size(code, imm_state_span(states[i]).min);
     }
-    emiss->offset[nstates] = size;
+    emis->offset[nstates] = size;
 
-    unsigned score_size = emis_score_size(emiss, nstates);
-    emiss->score = realloc(emiss->score, sizeof(*emiss->score) * score_size);
-    if (!emiss->score && score_size > 0)
+    unsigned score_size = emis_score_size(emis, nstates);
+    emis->score = realloc(emis->score, sizeof(*emis->score) * score_size);
+    if (!emis->score && score_size > 0)
     {
-        free(emiss->offset);
-        emiss->offset = NULL;
+        free(emis->offset);
+        emis->offset = NULL;
         return error(IMM_OUTOFMEM, "failed to resize");
     }
 
@@ -69,7 +68,7 @@ enum imm_rc emis_reset(struct imm_dp_emis *emiss,
                 j -= code_offset(code, min_seq);
                 imm_float score = imm_state_lprob(states[i], &seq);
                 assert(!imm_lprob_is_nan(score));
-                emiss->score[emiss->offset[i] + j] = score;
+                emis->score[emis->offset[i] + j] = score;
             }
         }
     }
