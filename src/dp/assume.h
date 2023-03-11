a02651d98aa64a1dc12a5609ae881f1f3ffd9259
Danilo Horta <danilo.horta@pm.me>
2023-03-11 10:07:58 +0000

    assume

--- src/dp/assume.h
+++ src/dp/assume.h
@@ -1,78 +1,45 @@
 #ifndef DP_ASSUME_H
 #define DP_ASSUME_H
 
+#include "dp/premise.h"
 #include "dp/state_type.h"
 
-#define ASSUME_MINLEN(T, MINLEN, x)                                            \
+#define ASSUME(x) __builtin_assume(x)
+
+#define ASSUME_MINLEN(P, x)                                                    \
     do                                                                         \
     {                                                                          \
-        __builtin_assume(UNKNOWN_STATE_ID <= T && T <= FRAME_STATE_ID);        \
-        if (T == UNKNOWN_STATE_ID)                                             \
-            __builtin_assume(0 <= x);                                          \
-        else if (T == MUTE_STATE_ID)                                           \
-            __builtin_assume(0 == x);                                          \
-        else if (T == NONMUTE_STATE_ID)                                        \
-            __builtin_assume(0 < x);                                           \
-        else if (T == NORMAL_STATE_ID)                                         \
-            __builtin_assume(1 == x);                                          \
-        else if (T == FRAME_STATE_ID)                                          \
-            __builtin_assume(MINLEN <= x);                                     \
+        ASSUME(UNKNOWN_STATE <= P.state && P.state <= FRAME_STATE);            \
+        if (P.state == UNKNOWN_STATE)                                          \
+            ASSUME(0 <= x);                                                    \
+        else if (P.state == MUTE_STATE)                                        \
+            ASSUME(0 == x);                                                    \
+        else if (P.state == NONMUTE_STATE)                                     \
+            ASSUME(0 < x);                                                     \
+        else if (P.state == NORMAL_STATE)                                      \
+            ASSUME(1 == x);                                                    \
+        else if (P.state == FRAME_STATE)                                       \
+            ASSUME(P.min_len <= x);                                            \
         else                                                                   \
             __builtin_unreachable();                                           \
     } while (0);
 
-#define ASSUME_MIXLEN(T, MINLEN, MAXLEN, x, y)                                 \
+#define ASSUME_MIXLEN(P, x, y)                                                 \
     do                                                                         \
     {                                                                          \
-        __builtin_assume(UNKNOWN_STATE_ID <= T && T <= FRAME_STATE_ID);        \
-        if (T == UNKNOWN_STATE_ID)                                             \
-            __builtin_assume(0 <= x && x <= y);                                \
-        else if (T == MUTE_STATE_ID)                                           \
-            __builtin_assume(0 == x && x == y);                                \
-        else if (T == NONMUTE_STATE_ID)                                        \
-            __builtin_assume(0 < x && x <= y);                                 \
-        else if (T == NORMAL_STATE_ID)                                         \
-            __builtin_assume(1 == x && x == y);                                \
-        else if (T == FRAME_STATE_ID)                                          \
-            __builtin_assume(MINLEN <= x && x <= y && y <= MAXLEN);            \
+        ASSUME(UNKNOWN_STATE <= P.state && P.state <= FRAME_STATE);            \
+        if (P.state == UNKNOWN_STATE)                                          \
+            ASSUME(0 <= x && x <= y);                                          \
+        else if (P.state == MUTE_STATE)                                        \
+            ASSUME(0 == x && x == y);                                          \
+        else if (P.state == NONMUTE_STATE)                                     \
+            ASSUME(0 < x && x <= y);                                           \
+        else if (P.state == NORMAL_STATE)                                      \
+            ASSUME(1 == x && x == y);                                          \
+        else if (P.state == FRAME_STATE)                                       \
+            ASSUME(P.min_len <= x && x <= y && y <= P.max_len);                \
         else                                                                   \
             __builtin_unreachable();                                           \
-    } while (0);
-
-#define ASSUME_REAL_PAST(T, src, dst)                                          \
-    do                                                                         \
-    {                                                                          \
-        if (T)                                                                 \
-            __builtin_assume(src <= dst);                                      \
-        else                                                                   \
-            __builtin_assume(0 == 0);                                          \
-    } while (0);
-
-#define ASSUME_FIRST_ROW(T, row)                                               \
-    do                                                                         \
-    {                                                                          \
-        if (T)                                                                 \
-            __builtin_assume(row == 0);                                        \
-        else                                                                   \
-            __builtin_assume(0 == 0);                                          \
-    } while (0);
-
-#define ASSUME_SAFE_FLOOR(T, len, row)                                         \
-    do                                                                         \
-    {                                                                          \
-        if (T)                                                                 \
-            __builtin_assume(len <= row);                                      \
-        else                                                                   \
-            __builtin_assume(0 == 0);                                          \
-    } while (0);
-
-#define ASSUME_SAFE_CEIL(T, last_row, stop_row)                                \
-    do                                                                         \
-    {                                                                          \
-        if (T)                                                                 \
-            __builtin_assume(last_row <= stop_row);                            \
-        else                                                                   \
-            __builtin_assume(0 == 0);                                          \
     } while (0);
 
 #endif
