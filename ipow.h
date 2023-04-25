e24142905f8beec50e9eb376fb1b300785258ff9
Danilo Horta <danilo.horta@pm.me>
2023-04-25 15:05:29 +0100

    fix for gcc

--- ipow.h
+++ ipow.h
@@ -3,7 +3,7 @@
 
 #include "compiler.h"
 
-CONST_ATTR TEMPLATE unsigned long imm_ipow(unsigned long x, unsigned e)
+CONST_ATTR static inline unsigned long imm_ipow(unsigned long x, unsigned e)
 {
   return !e ? 1 : (e & 1 ? x : 1) * imm_ipow(x * x, e >> 1);
 }
