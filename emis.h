95b91df75534c758de6fd0786f6e1d758ff1a24e
Danilo Horta <danilo.horta@pm.me>
2023-07-09 11:40:51 +0100

    Refactor imm_emis_dump function parameters

--- emis.h
+++ emis.h
@@ -39,7 +39,7 @@ imm_template float const *imm_emis_table(struct imm_emis const *x,
 }
 
 IMM_API void imm_emis_dump(struct imm_emis const *,
-                           struct imm_state_table const *y, imm_state_name *,
+                           struct imm_state_table const *, imm_state_name *,
                            FILE *restrict);
 
 #endif
