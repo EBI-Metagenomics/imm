ed1e67352f23e7898d8195aafbd0644901beb1d6
Danilo Horta <danilo.horta@pm.me>
2023-10-13 11:40:08 +0100

    int

--- code.c
+++ code.c
@@ -2,27 +2,27 @@
 #include "abc.h"
 #include "seq.h"
 
-static void calc_strides(uint16_t *stride, unsigned abc_size)
+static void calc_strides(int16_t *stride, int abc_size)
 {
-  int last = (int)(IMM_STATE_MAX_SEQLEN - 1);
+  int last = IMM_STATE_MAX_SEQLEN - 1;
   stride[last] = 1;
   while (last > 0)
   {
     --last;
-    stride[last] = (uint16_t)(stride[last + 1] * abc_size);
+    stride[last] = (int16_t)(stride[last + 1] * abc_size);
   }
 }
 
-static void calc_offsets(uint16_t *offset, unsigned abc_size)
+static void calc_offsets(int16_t *offset, int abc_size)
 {
-  unsigned n = abc_size;
+  int n = abc_size;
   int i = 0;
   offset[i] = 0;
   /* +1 for sentinel */
-  while (i < (int)(IMM_STATE_MAX_SEQLEN + 1))
+  while (i < IMM_STATE_MAX_SEQLEN + 1)
   {
     ++i;
-    offset[i] = (uint16_t)(offset[i - 1] * n + 1);
+    offset[i] = (int16_t)(offset[i - 1] * n + 1);
   }
 }
 
@@ -33,14 +33,14 @@ void imm_code_init(struct imm_code *code, struct imm_abc const *abc)
   calc_offsets(code->offset, imm_abc_size(code->abc));
 }
 
-unsigned imm_code_encode(struct imm_code const *code, struct imm_seq const *seq)
+int imm_code_encode(struct imm_code const *code, struct imm_seq const *seq)
 {
-  unsigned len = imm_seq_size(seq);
-  unsigned c = code->offset[len];
-  for (unsigned i = 0; i < len; ++i)
+  int size = imm_seq_size(seq);
+  int c = code->offset[size];
+  for (int i = 0; i < size; ++i)
   {
-    unsigned j = imm_seq_symbol_idx(seq, i);
-    c += code->stride[IMM_STATE_MAX_SEQLEN - len + i] * j;
+    int j = imm_seq_symbol_idx(seq, i);
+    c += code->stride[IMM_STATE_MAX_SEQLEN - size + i] * j;
   }
   return c;
 }
--- code.h
+++ code.h
@@ -12,25 +12,24 @@ struct imm_seq;
 
 struct imm_code
 {
-  uint16_t offset[IMM_STATE_MAX_SEQLEN + 2];
-  uint16_t stride[IMM_STATE_MAX_SEQLEN];
+  int16_t offset[IMM_STATE_MAX_SEQLEN + 2];
+  int16_t stride[IMM_STATE_MAX_SEQLEN];
   struct imm_abc const *abc;
 };
 
 IMM_API void imm_code_init(struct imm_code *, struct imm_abc const *);
-IMM_API unsigned imm_code_encode(struct imm_code const *,
-                                 struct imm_seq const *);
+IMM_API int imm_code_encode(struct imm_code const *, struct imm_seq const *);
 
-IMM_PURE unsigned imm_code_translate(struct imm_code const *x, unsigned value,
-                                     unsigned min_seq)
+IMM_PURE int imm_code_translate(struct imm_code const *x, int value,
+                                int min_seq)
 {
   assert(value >= x->offset[min_seq]);
   return value - x->offset[min_seq];
 }
 
-IMM_PURE unsigned imm_code_size(struct imm_code const *x, struct imm_span span)
+IMM_PURE int imm_code_size(struct imm_code const *x, struct imm_span span)
 {
-  return (unsigned)(x->offset[span.max + 1] - x->offset[span.min]);
+  return x->offset[span.max + 1] - x->offset[span.min];
 }
 
 #endif
