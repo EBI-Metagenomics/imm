4b800ac2d601a737a4eac073ac53559356f83607
Danilo Horta <danilo.horta@pm.me>
2023-10-25 14:57:04 +0100

    Refactor imm_eseq_slice to use a pointer arithmetic formula

--- eseq.c
+++ eseq.c
@@ -44,7 +44,7 @@ int imm_eseq_setup(struct imm_eseq *eseq, struct imm_seq const *seq)
 struct imm_eseq imm_eseq_slice(struct imm_eseq const *x, struct imm_range r)
 {
   assert(r.start + imm_range_size(r) + 1 <= x->data.rows);
-  int16_t *ptr = x->data.data + r.start;
+  int16_t *ptr = x->data.data + r.start * x->data.cols;
   struct imm_matrixi16 m = {ptr, imm_range_size(r) + 1, x->data.cols};
   return (struct imm_eseq){m, x->code};
 }
